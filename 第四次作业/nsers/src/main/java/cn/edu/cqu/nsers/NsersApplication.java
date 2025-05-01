6a15e238f237d7de5e71d1efb337256a832470c9
Haoyan-collab <2030537847.qq.com>
2025-05-01 23:50:03 +0800

    fourth assignment

---
+++ 第四次作业/nsers/src/main/java/cn/edu/cqu/nsers/NsersApplication.java
@@ -0,0 +1,14 @@
+package cn.edu.cqu.nsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NsersApplication.class, args);
+    }
+
+}
+//登录：http://localhost:8080/startPage
